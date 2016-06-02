47addf85d9ce6ece5a69d165675ce95558900950
stormslowly@gmail.com <stormslowly@gmail.com>
2016-06-02 23:10:04 +0800

    cover is movable

--- shared/components/Dashboard.js
+++ shared/components/Dashboard.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ProjectCover from  './ProjectCover'
-
+import Sortable   from 'sortablejs'
 import {Link} from 'react-router'
 
 function NewProject() {
@@ -9,25 +9,47 @@ function NewProject() {
   </div>
 }
 
-function Projects(props) {
-  var projects = props.projects
-  return <div className='clearfix'>
-    <div className="projects" >
-      {projects.map((p, i)=> <Link key={i} to={`/project/${i}`}>
-        <ProjectCover project={p} remove={ function(){props.removeProject(i)} }/></Link>)
-      }
-      <Link className="new-project" to={'/project/new'}> <NewProject/> </Link>
-    </div>
-  </div>
-}
+class Projects extends React.Component {
+  constructor(props, context) {
+    super(props, context)
+    this.sortableContainersDecorator = this.sortableContainersDecorator.bind(this)
+  }
 
+  sortableContainersDecorator(componentBackingInstance) {
+    if (componentBackingInstance) {
+      let options = {
+        handle: ".projectHandle"
+      };
+      Sortable.create(componentBackingInstance, options);
+    }
+  }
+
+  render() {
+    var projects = this.props.projects
+    var removeProject = this.props.removeProject
+    return <div className='clearfix'>
+      <div className="projects" ref={this.sortableContainersDecorator}>
+        {
+          projects.map((p, i)=> <div className="projectHandle">
+            <Link key={i} to={`/project/${i}`}>
+                <ProjectCover project={p} remove={ function(){removeProject(i)} }/>
+              </Link>
+            </div>
+          )
+        }
+        <Link className="new-project" to={'/project/new'}> <NewProject/> </Link>
+      </div>
+    </div>
+  }
+}
 
 class Dashboard extends React.Component {
   constructor(props, context) {
     super(props, context)
   }
+
   render() {
-    return <Projects projects={this.props.projects}  removeProject={this.props.removeProject}/>
+    return <Projects projects={this.props.projects} removeProject={this.props.removeProject}/>
   }
 }
 
